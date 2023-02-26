c61bc8c7f71396669f8e29670c9fa5ff410d1db1
alirezamou <am808036@gmail.com>
2023-02-26 15:05:29 +0330

    add Lato font from fonts.googleapis

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
