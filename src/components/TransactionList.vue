68967d7a4fcd7d4f06afca0f9aa01f796b204614
alirezamou <am808036@gmail.com>
2023-02-27 00:51:30 +0330

    wrap TransactionList and Balance with div

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,13 +1,15 @@
 <template>
-    <h2 class="font-bold text-xl border-b-2 border-solid border-[#dedede]">History</h2>
-    <ul class="mt-2">
-        <li class="bg-white my-2 flex justify-between p-2 shadow rounded-2 border-r-4 border-red-500">
-            <span>Banana  </span>
-            <span>-300$</span>
-        </li>
-        <li class="bg-white my-2 flex justify-between p-2 shadow rounded-2 border-r-4 border-green-500">
-            <span>Cash  </span>
-            <span>+500$</span>
-        </li>
-    </ul>
+    <div>
+        <h2 class="font-bold text-xl border-b-2 border-solid border-[#dedede]">History</h2>
+        <ul class="mt-2">
+            <li class="bg-white my-2 flex justify-between p-2 shadow rounded-2 border-r-4 border-red-500">
+                <span>Banana  </span>
+                <span>-300$</span>
+            </li>
+            <li class="bg-white my-2 flex justify-between p-2 shadow rounded-2 border-r-4 border-green-500">
+                <span>Cash  </span>
+                <span>+500$</span>
+            </li>
+        </ul>
+    </div>
 </template>
