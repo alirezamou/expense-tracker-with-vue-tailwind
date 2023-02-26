25911c9aa734f71d5a396847a03ed5c4785bf4fc
alirezamou <am808036@gmail.com>
2023-02-26 15:48:11 +0330

    add IncomeExpense comp

---
+++ src/components/IncomeExpense.vue
@@ -0,0 +1,12 @@
+<template>
+    <div className="flex justify-between gap-4 bg-white shadow p-[20px] my-[20px] mx-0">
+        <div className="flex-1 text-center border-r border-solid border-[#dedede]">
+            <h2 className="font-bold uppercase text-xl">Income</h2>
+            <p className="text-green-600 text-xl">+0.00</p>
+        </div>
+        <div className="flex-1 text-center">
+            <h2 className="font-bold uppercase text-xl">Expense</h2>
+            <p className="text-red-600 text-xl">-0.00</p>
+        </div>
+    </div>
+</template>
