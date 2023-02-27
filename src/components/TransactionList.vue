55dc19fb5dc5d7bb7d473cb409f1f9c26a45c19b
alirezamou <am808036@gmail.com>
2023-02-27 12:49:06 +0330

    add delete button

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -17,11 +17,14 @@ export default {
         <ul class="mt-2">
             <li v-for="trx in transactions"
              :id="trx.id" 
-             class="bg-white my-2 flex justify-between p-2 shadow rounded-2 border-r-4"
+             class="group relative bg-white my-2 flex justify-between p-2 shadow rounded-2 border-r-4"
              :class="trx.amount >= 0 ? 'border-green-500' : 'border-red-500'"
             >
                 <span>{{ trx.text }}</span>
                 <span>{{ trx.amount > 0 ? `+${trx.amount}` : trx.amount }}$</span>
+                <button
+                class="absolute top-1/2 left-0 -translate-y-1/2 -translate-x-full px-2 py-0.5 bg-[#e74c3c] text-white rounded-sm opacity-0 group-hover:opacity-100 transition-opacity ease duration-300"
+                >x</button>
             </li>
         </ul>
     </div>
