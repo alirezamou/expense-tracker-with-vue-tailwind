a9d2d0c76527d7d07f0ff721b4e659ba95468f09
alirezamou <am808036@gmail.com>
2023-02-27 12:14:03 +0330

    populate TransactionList with datas from store

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,14 +1,26 @@
+<script>
+import { useTransactionStore } from '../stores/transaction';
+export default {
+    setup() {
+        const transactionStore = useTransactionStore();
+        return { transactionStore };
+    },
+    computed: {
+        transactions() { return this.transactionStore.transactions; }
+    }
+}
+</script>
+
 <template>
     <div>
         <h2 class="font-bold text-xl border-b-2 border-solid border-[#dedede]">History</h2>
         <ul class="mt-2">
-            <li class="bg-white my-2 flex justify-between p-2 shadow rounded-2 border-r-4 border-red-500">
-                <span>Banana  </span>
-                <span>-300$</span>
-            </li>
-            <li class="bg-white my-2 flex justify-between p-2 shadow rounded-2 border-r-4 border-green-500">
-                <span>Cash  </span>
-                <span>+500$</span>
+            <li v-for="trx in transactions"
+             :id="trx.id" 
+             class="bg-white my-2 flex justify-between p-2 shadow rounded-2 border-r-4 border-red-500"
+            >
+                <span>{{ trx.text }}</span>
+                <span>{{ trx.amount }}$</span>
             </li>
         </ul>
     </div>
