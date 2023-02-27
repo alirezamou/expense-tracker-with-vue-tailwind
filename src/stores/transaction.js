324fd0db4f70501047e2edbed72e56dc86027d54
alirezamou <am808036@gmail.com>
2023-02-27 11:30:20 +0330

    add transaction store with dummy transactions

---
+++ src/stores/transaction.js
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+
+export const useTransactionStore = defineStore("transactions", {
+  state: () => ({
+    // dummy transactions
+    transactions: [
+      { id: 1, text: "Flower", amount: -20 },
+      { id: 2, text: "Salary", amount: 300 },
+      { id: 3, text: "Book", amount: -10 },
+      { id: 4, text: "Camera", amount: 150 },
+    ],
+  }),
+});
