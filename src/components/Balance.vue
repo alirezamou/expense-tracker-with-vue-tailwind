4f6027e949e3f26cb646cc40d352c841dab79b13
alirezamou <am808036@gmail.com>
2023-02-27 12:29:22 +0330

    calculate balance of trxs

--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,6 +1,23 @@
+<script>
+import { useTransactionStore } from '../stores/transaction';
+export default {
+    setup() {
+        const transactionStore = useTransactionStore();
+        return { transactionStore };
+    },
+    computed: {
+        balanceAmount() {
+            const amounts = this.transactionStore.transactions.map(trx => trx.amount);
+            return amounts.reduce((acc, curVal) => acc += curVal, 0);
+        }
+    }
+}
+
+</script>
+
 <template>
     <div>
         <h1 class="uppercase">Your balance is: </h1>
-        <h4 class="font-bold text-3xl tracking-wider">$00.00</h4>
+        <h4 class="font-bold text-3xl tracking-wider">{{ balanceAmount > 0 ? `+${balanceAmount}` : balanceAmount }}</h4>
     </div>
 </template>
