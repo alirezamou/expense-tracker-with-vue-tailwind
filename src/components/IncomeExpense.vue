aa40c038f8e770bb4705d87e7a4e286ea858fc4f
alirezamou <am808036@gmail.com>
2023-02-27 12:05:59 +0330

    populate IncomeExpense comp with datas from store

--- src/components/IncomeExpense.vue
+++ src/components/IncomeExpense.vue
@@ -1,12 +1,27 @@
+<script>
+import { useTransactionStore } from '../stores/transaction';
+export default {
+    setup() {
+        const transactionStore = useTransactionStore();
+        return { transactionStore };
+    },
+    computed: {
+        amounts() { return this.transactionStore.transactions.map(trx => trx.amount) },
+        incomeAmount() { return this.amounts.filter(amnt => amnt > 0).reduce((acc, curVal) => acc += curVal, 0) },
+        expenseAmount() { return this.amounts.filter(amnt => amnt < 0).reduce((acc, curVal) => acc += curVal, 0) },
+    }
+}
+</script>
+
 <template>
     <div class="flex justify-between gap-4 bg-white shadow p-[20px] my-[20px] mx-0">
         <div class="flex-1 text-center border-r border-solid border-[#dedede]">
             <h2 class="font-bold uppercase text-xl">Income</h2>
-            <p class="text-green-600 text-xl">+0.00</p>
+            <p class="text-green-600 text-xl">{{ incomeAmount ? `+${incomeAmount}` : '0' }}</p>
         </div>
         <div class="flex-1 text-center">
             <h2 class="font-bold uppercase text-xl">Expense</h2>
-            <p class="text-red-600 text-xl">-0.00</p>
+            <p class="text-red-600 text-xl">{{ expenseAmount }}</p>
         </div>
     </div>
 </template>
